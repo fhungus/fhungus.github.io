f2881db4abd3816f7324a2f767ed324d5f62e09a
fhungus <crayfish@duck.com>
2026-05-23 21:16:12 +0100

    stole this fucking fish --> < ><3

--- src/components/affirmations/affirmations.svelte
+++ src/components/affirmations/affirmations.svelte
@@ -2,17 +2,17 @@
     // copied my shit ass code here as well
     import { onMount } from "svelte";
     import { blur } from "svelte/transition";
-    import { on } from 'svelte/events';
+    import { on } from "svelte/events";
     import { Spring } from "svelte/motion";
 
-    import range from '../../lib/range'
-    
+    import range from "../../lib/range";
+
     let active = $state(localStorage.getItem("fish_paused") != "n");
     let offset = new Spring(0);
 
     // i have GOTTA start abstracting some of this shit!!!
     class AffirmationEntity {
-        variant: 1 | 2 | 3 = 1;
+        variant: 1 | 2 | 3 | 4 = 1;
         vx = 0;
         vy = 0;
         x = 0;
@@ -20,42 +20,51 @@
         opacity = 0;
 
         constructor(w: number, h: number) {
-            let num: 1 | 2 | 3 = 1;
-            let guhhihatejs = range(Math.random(), 0,1, 0,3);
-            if (guhhihatejs > 2) {
-                num = 3
+            let num: 1 | 2 | 3 | 4 = 1;
+            let guhhihatejs = range(Math.random(), 0, 1, 0, 3.1);
+            if (guhhihatejs > 3) {
+                num = 4;
+            } else if (guhhihatejs > 2) {
+                num = 3;
             } else if (guhhihatejs > 1) {
-                num = 2
+                num = 2;
             } else {
-                num = 1
+                num = 1;
             }
             this.variant = num;
 
-            this.vx = range(Math.random(), 0,1, -500, 500);
-            this.vy = range(Math.random(), 0,1, -500, 500);
+            this.vx = range(Math.random(), 0, 1, -500, 500);
+            this.vy = range(Math.random(), 0, 1, -500, 500);
 
-            this.x = range(Math.random(), 0,1, 0,w); 
-            this.y = range(Math.random(), 0,1, 0,h); 
+            this.x = range(Math.random(), 0, 1, 0, w);
+            this.y = range(Math.random(), 0, 1, 0, h);
 
-            this.opacity = range(Math.random(), 0,1, 0.25,1);
+            this.opacity = range(Math.random(), 0, 1, 0.25, 1);
         }
 
-        stepFrame(dt: number, w: number, h: number, mouse: MousePosition, others: AffirmationEntity[]) {
+        stepFrame(
+            dt: number,
+            w: number,
+            h: number,
+            mouse: MousePosition,
+            others: AffirmationEntity[],
+        ) {
             this.x += this.vx * dt;
             this.y += this.vy * dt;
 
             this.vy += 500 * dt;
 
             // bounce off ground & ceiling
-            if (this.y > h && this.vy > 0 || this.y < 15 && this.vy < 0) {// EPIC magic number....
-                this.y = this.y > h ? h : 20
+            if ((this.y > h && this.vy > 0) || (this.y < 15 && this.vy < 0)) {
+                // EPIC magic number....
+                this.y = this.y > h ? h : 20;
                 this.vy = this.vy * -0.8;
             }
 
             // bounce from sides
-            if (this.x <= 0 && this.vx < 0 || this.x >= w  && this.vx > 0) {
+            if ((this.x <= 0 && this.vx < 0) || (this.x >= w && this.vx > 0)) {
                 // push the frames springs a bit
-                offset.set(offset.current + this.vx * 0.001, {instant: true});
+                offset.set(offset.current + this.vx * 0.001, { instant: true });
                 offset.set(0);
 
                 this.vx *= -1;
@@ -66,14 +75,22 @@
             const COLLISION_DISPLACEMENT = 200 * dt;
             others.map((other) => {
                 if (Math.abs(this.x - other.x) < RANGE) {
-                    this.vx += COLLISION_DISPLACEMENT * (this.x - other.x > 0 ? 1 : -1)
-                    other.vx -= COLLISION_DISPLACEMENT * (this.y - other.y > 0 ? 1 : -1)
+                    this.vx +=
+                        COLLISION_DISPLACEMENT *
+                        (this.x - other.x > 0 ? 1 : -1);
+                    other.vx -=
+                        COLLISION_DISPLACEMENT *
+                        (this.y - other.y > 0 ? 1 : -1);
                 }
                 if (Math.abs(this.y - other.y) < RANGE) {
-                    this.vy += COLLISION_DISPLACEMENT * (this.y - other.y > 0 ? 1 : -1)
-                    other.vy -= COLLISION_DISPLACEMENT * (this.y - other.y > 0 ? 1 : -1)
+                    this.vy +=
+                        COLLISION_DISPLACEMENT *
+                        (this.y - other.y > 0 ? 1 : -1);
+                    other.vy -=
+                        COLLISION_DISPLACEMENT *
+                        (this.y - other.y > 0 ? 1 : -1);
                 }
-            })
+            });
 
             let vx_sign = this.vx > 0 ? 1 : -1;
             let vy_sign = this.vy > 0 ? 1 : -1;
@@ -88,38 +105,41 @@
                 const RANGE = 1000;
                 let decent_gaussian = (x: number) => {
                     const PEAK = 2500;
-                    let gaussian = PEAK * Math.exp(-((x^2) / (RANGE ^2)))
-                    gaussian -= (PEAK * 1.2) * Math.exp(-((x^2)/((RANGE/10)^2))) // THUNK!
-                    return gaussian
-                }
+                    let gaussian = PEAK * Math.exp(-((x ^ 2) / (RANGE ^ 2)));
+                    gaussian -=
+                        PEAK * 1.2 * Math.exp(-((x ^ 2) / ((RANGE / 10) ^ 2))); // THUNK!
+                    return gaussian;
+                };
                 if (Math.abs(dx) < RANGE) {
-                    this.vx -= decent_gaussian(dx) * dt
+                    this.vx -= decent_gaussian(dx) * dt;
                 }
                 if (Math.abs(dy) < RANGE) {
                     //console.log(`${this.y} - ${mouse.y} = ${dy}`)
-                    this.vy -= decent_gaussian(dy) * dt
+                    this.vy -= decent_gaussian(dy) * dt;
                 }
             }
 
             // THE FUCKING SPEED LIMIT!!!
-            const SPEED_LIMIT = 700
-            if (this.vx > SPEED_LIMIT || this.vx < -SPEED_LIMIT) { // theres no math.clamp function, how sad...
+            const SPEED_LIMIT = 700;
+            if (this.vx > SPEED_LIMIT || this.vx < -SPEED_LIMIT) {
+                // theres no math.clamp function, how sad...
                 this.vx = SPEED_LIMIT * vx_sign;
             }
-            if (this.vy > SPEED_LIMIT || this.vy < -SPEED_LIMIT) { // theres no math.clamp function, how sad...
+            if (this.vy > SPEED_LIMIT || this.vy < -SPEED_LIMIT) {
+                // theres no math.clamp function, how sad...
                 this.vy = SPEED_LIMIT * vy_sign;
             }
         }
     }
 
     class Affirmations {
-        affirmations: AffirmationEntity[] = []
+        affirmations: AffirmationEntity[] = [];
         width: number = 0;
         height: number = 0;
         constructor(w: number, h: number) {
             this.width = w;
             this.height = h;
-            
+
             const affirmations = 50;
             for (var i = 0; i < affirmations; i++) {
                 let affirmation_entity = new AffirmationEntity(w, h);
@@ -128,12 +148,18 @@
         }
 
         stepFrame(dt: number, mouse: MousePosition) {
-            this.affirmations.forEach(element => {
-                element.stepFrame(dt, this.width, this.height, mouse, this.affirmations)
+            this.affirmations.forEach((element) => {
+                element.stepFrame(
+                    dt,
+                    this.width,
+                    this.height,
+                    mouse,
+                    this.affirmations,
+                );
             });
         }
     }
-    
+
     class MousePosition {
         x = 0;
         y = 0;
@@ -163,14 +189,14 @@
             mousex = e.clientX - rect.x;
             mousey = e.clientY - rect.y;
 
-            console.log(`${e.clientY} - ${rect.y} = ${mousey}`)
-        })
+            console.log(`${e.clientY} - ${rect.y} = ${mousey}`);
+        });
         on(canvas, "mouseenter", (_) => {
-            mouseincanvas = true
-        })
+            mouseincanvas = true;
+        });
         on(canvas, "mouseleave", (_) => {
-            mouseincanvas = false
-        })
+            mouseincanvas = false;
+        });
 
         let last_scroll_position = window.scrollY;
         document.addEventListener("scroll", (_) => {
@@ -178,61 +204,97 @@
             last_scroll_position = window.scrollY;
 
             affirmations.affirmations.map((aff, _) => {
-                aff.y += delta
-                aff.vy += delta * 2
-            })
-        })
+                aff.y += delta;
+                aff.vy += delta * 2;
+            });
+        });
 
         let ctx: CanvasRenderingContext2D = canvas.getContext("2d")!;
         let drawingTimeout: NodeJS.Timeout;
         let onTimeout = (tick: number) => {
             let now = new Date().getTime();
-            let mouse = new MousePosition(mousex, mousey, mouseincanvas)
+            let mouse = new MousePosition(mousex, mousey, mouseincanvas);
             if (active) {
-            let rect = canvas.getBoundingClientRect();
+                let rect = canvas.getBoundingClientRect();
                 canvas.height = rect.height;
                 canvas.width = rect.width;
                 affirmations.height = rect.height;
                 affirmations.width = rect.width;
-                affirmations.stepFrame((now - tick) / 1000, mouse)
-                ctx.clearRect(0,0,canvas.width,canvas.height);
+                affirmations.stepFrame((now - tick) / 1000, mouse);
+                ctx.clearRect(0, 0, canvas.width, canvas.height);
                 ctx.font = `bold 14px GoMono`;
 
-                const TEXT_HEIGHT = 10
-                const ROUNDING = 1
+                const TEXT_HEIGHT = 10;
+                const ROUNDING = 1;
                 affirmations.affirmations.map((aff, _) => {
-                    ctx.fillStyle = `rgba(${aff.variant == 1 ? "116,225,195" : aff.variant == 2 ? "198,209,142" : "255,255,92"}, ${aff.opacity})`
-                    let forwards = aff.variant == 1 ? "< ><" : aff.variant == 2 ? "{ }:{" : "< >::"
-                    let backwards = aff.variant == 1 ? ">< >" : aff.variant == 2 ? "}:{ }" : "::< >"
-                    ctx.fillText(`${aff.vx < 0 ? forwards : backwards}`, Math.round(aff.x/ROUNDING)*ROUNDING, Math.round((aff.y - (TEXT_HEIGHT / 2)) / ROUNDING) * ROUNDING)
-                })
+                    // highkey should be using a map for these
+                    ctx.fillStyle = `rgba(${aff.variant == 1 ? "116,225,195" : aff.variant == 2 ? "198,209,142" : aff.variant == 3 ? "255,255,92" : "245, 202, 199"}, ${aff.opacity})`;
+                    let forwards =
+                        aff.variant == 1
+                            ? "< ><"
+                            : aff.variant == 2
+                              ? "{ }:{"
+                              : aff.variant == 3
+                                ? "< >::"
+                                : "< ><3";
+                    let backwards =
+                        aff.variant == 1
+                            ? ">< >"
+                            : aff.variant == 2
+                              ? "}:{ }"
+                              : aff.variant == 3
+                                ? "::< >"
+                                : "Ɛ>< >";
+                    ctx.fillText(
+                        `${aff.vx < 0 ? forwards : backwards}`,
+                        Math.round(aff.x / ROUNDING) * ROUNDING,
+                        Math.round((aff.y - TEXT_HEIGHT / 2) / ROUNDING) *
+                            ROUNDING,
+                    );
+                });
             }
 
-            drawingTimeout = setTimeout(onTimeout, 1000/45, new Date().getTime());
-        }
+            drawingTimeout = setTimeout(
+                onTimeout,
+                1000 / 45,
+                new Date().getTime(),
+            );
+        };
         onTimeout(new Date().getTime());
 
         return () => {
-            drawingTimeout.close()
-        }
-    })
+            drawingTimeout.close();
+        };
+    });
 </script>
-<div class="affirmations" style="margin-left:{offset.current}px; margin-right:{-offset.current}px;">
-    <button class="affirmations_button" onclick={()=>{
-        active=!active
-        localStorage.setItem("fish_paused", active ? "y" : "n")
-    }}>
+
+<div
+    class="affirmations"
+    style="margin-left:{offset.current}px; margin-right:{-offset.current}px;"
+>
+    <button
+        class="affirmations_button"
+        onclick={() => {
+            active = !active;
+            localStorage.setItem("fish_paused", active ? "y" : "n");
+        }}
+    >
         <div>
             {active ? "):<" : "(:"}
         </div>
     </button>
     {#if !active}
-    <div transition:blur class="pause_text"> <!--appears when the fish are paused...-->
-        Your fish have been paused.<br/><br/>
-        This thing is here because I make them do a lot of wacky calculations and they might be particulary lag inducing.
-    </div>
+        <div transition:blur class="pause_text">
+            <!--appears when the fish are paused...-->
+            Your fish have been paused.<br /><br />
+            This thing is here because I make them do a lot of wacky calculations
+            and they might be particulary lag inducing.
+        </div>
     {/if}
-    <canvas bind:this={canvas} class={`affirmations_canvas ${!active && "halfopacity"}`}></canvas>
+    <canvas
+        bind:this={canvas}
+        class={`affirmations_canvas ${!active && "halfopacity"}`}
+    ></canvas>
 </div>
 
 <style>
@@ -268,18 +330,18 @@
     }
 
     .pause_text {
-        align-self:center;
+        align-self: center;
         text-align: center;
         position: absolute;
 
         padding: 2em;
-        font-size:0.75em;
+        font-size: 0.75em;
 
         color: #546b5f;
     }
 
     .affirmations_button {
-        transition: all linear .2s;
+        transition: all linear 0.2s;
 
         position: absolute;
         top: 0;
@@ -304,7 +366,7 @@
         font-weight: bold;
 
         border-radius: 0 5px 0 5px;
-        
+
         z-index: 10;
     }
 
@@ -318,5 +380,4 @@
         background-color: var(--fg);
         color: var(--bg2);
     }
-
 </style>
